ac99a9bc3ad85f3286cbdea6083f18b69f72d77f
itsmvx <xenovzx@gmail.com>
2023-11-30 15:12:13 +0700

    Static Image assets added

---
+++ lib/StaticImages.tsx
@@ -0,0 +1,74 @@
+import Haruna1L from "@/public/assets/7a27660bedbdbbc4b8a9125ed546f7b8.webp";
+import Haruna2L from "@/public/assets/8244049dd7746ef76bf566cc1ca50a02.webp";
+import Haruna3L from "@/public/assets/92443010ed7746ef76bf566cc1ca50a02.webp";
+import Haruna4L from "@/public/assets/dec12f052b66040e64714a18354ea03d.webp";
+import Noa1L from "@/public/assets/91e0bdf8e4a8af77250a9f6dbedc0bf2.webp";
+import Noa2L from "@/public/assets/20230702_185119.webp";
+import Noa3L from "@/public/assets/20230710_090318.webp";
+import Mari1L from "@/public/assets/20230715_1315444.webp";
+import Mari2L from "@/public/assets/20230815_130224.webp";
+import Rio1L from "@/public/assets/20230830_205049.webp";
+import Rio2L from "@/public/assets/20231002_105450.webp";
+import Shiroko1L from "@/public/assets/20230905_150623.webp";
+import AronaPlana1P from "@/public/assets/20230909_213827.webp";
+import HaloFest1L from "@/public/assets/36e1064e4ded68c9017a024cfe408dad.webp";
+import HaloFest2L from "@/public/assets/6df6a8e49e15bd7dc8a343a567ca59f3.webp";
+import AbibasLogo from "@/public/assets/abibas-logo.webp";
+import LoadingSpinner from "@/public/assets/loading-spinner.svg";
+import { StaticImageData } from "next/image";
+export const ImagesAll: StaticImageData[] = [
+    Haruna1L,
+    Haruna2L,
+    Haruna3L,
+    Haruna4L,
+    Noa1L,
+    Noa2L,
+    Noa3L,
+    Mari1L,
+    Mari2L,
+    Rio1L,
+    Rio2L,
+    Shiroko1L,
+    AronaPlana1P,
+    HaloFest1L,
+    HaloFest2L,
+];
+
+export {
+    AbibasLogo,
+    Haruna1L,
+    Haruna2L,
+    Haruna3L,
+    Haruna4L,
+    Noa1L,
+    Noa2L,
+    Noa3L,
+    Mari1L,
+    Mari2L,
+    Rio1L,
+    Rio2L,
+    Shiroko1L,
+    AronaPlana1P,
+    HaloFest1L,
+    HaloFest2L,
+    LoadingSpinner,
+};
+export const ImagesHaruna: StaticImageData[] = [
+    Haruna1L,
+    Haruna2L,
+    Haruna3L,
+    Haruna4L,
+];
+export const ImagesMari: StaticImageData[] = [
+    Mari1L,
+    Mari2L,
+];
+export const ImagesNoa: StaticImageData[] = [
+    Noa1L,
+    Noa2L,
+    Noa3L,
+];
+export const ImagesRio: StaticImageData[] = [
+    Rio1L,
+    Rio2L,
+];
