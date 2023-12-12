8aa0217a280dd85ccbbac3f59cbf4d3bff9d3fb6
itsmvx <xenovzx@gmail.com>
2023-12-13 03:23:56 +0700

    delete intrinsic.d.ts, now all types includes in types.d.ts

--- intrinsic.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'iconify-icon': React.DetailedHTMLProps<React.HTMLAttributes, HTMLElement>
-        }
-    }
-}
