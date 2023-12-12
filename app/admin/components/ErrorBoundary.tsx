c61437db52298cfe03bb891e2b0acb9785fbb87e
itsmvx <xenovzx@gmail.com>
2023-12-13 03:10:01 +0700

    Admin Universal Components add

--- app/admin/components/ErrorBoundary.tsx
+++ app/admin/components/ErrorBoundary.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Image from "next/image";
 import { Chicken } from "@/lib/StaticImages";
+import { useRouter } from "next/navigation";
 
 export const ErrorBoundary = () => {
+    const router = useRouter();
     return (
         <>
             <div className="w-full h-96 flex flex-col gap-y-4 items-center justify-center">
@@ -13,6 +16,14 @@ export const ErrorBoundary = () => {
                 <p className="text-lg font-medium ">
                     Oops.. Nothing here except chicken
                 </p>
+                <button
+                    onClick={() => {
+                        router.refresh();
+                    }}
+                    className="group bg-neutral-900 px-2 py-1 flex items-center justify-center gap-x-1 text-sm text-white rounded-md">
+                    Reload
+                    <iconify-icon width={20} class="group-focus:animate-spin" icon="mdi:autorenew"></iconify-icon>
+                </button>
             </div>
         </>
     );
--- app/admin/error.tsx
+++ app/admin/error.tsx
@@ -1,7 +1,31 @@
 'use client'
-const ErrorPage = () => {
+import Image from "next/image";
+import { Chicken } from "@/lib/StaticImages";
+import { useRouter } from "next/navigation";
+
+export default function Error() {
+    const router = useRouter();
     return (
-        <div>ErrorPage</div>
-    )
+        <>
+            <div className="w-full h-96 flex flex-col gap-y-4 items-center justify-center">
+                <Image
+                    src={Chicken}
+                    alt="error chicken"
+                    width={150}
+                />
+                <p className="text-lg font-medium ">
+                    Oops.. Nothing here except chicken
+                </p>
+                <button
+                    onClick={() => {
+                        router.refresh();
+                    }}
+                    className="group bg-neutral-900 px-2 py-1 flex items-center justify-center gap-x-1 text-sm text-white rounded-md">
+                    Reload
+                    <iconify-icon width={20} class="group-focus:animate-spin" icon="mdi:autorenew"></iconify-icon>
+                </button>
+            </div>
+        </>
+    );
 }
-export default ErrorPage
+
