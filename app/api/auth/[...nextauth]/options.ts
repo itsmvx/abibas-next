6743f5ebe20113d809af5458c8994a6299ad03f9
itsmvx <xenovzx@gmail.com>
2023-12-13 03:12:31 +0700

    Next Auth fix middleware and Auth Logic

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -13,21 +13,36 @@ export const options: NextAuthOptions = {
                 username: { label: "Username", type: "text" },
                 password: { label: "Password", type: "password" }
             },
-            async authorize(credentials) {
+            async authorize(credentials: Record<"username" | "password", string> | undefined) {
                 const { username, password } = credentials ?? {}
                 if (!username || !password) {
                     throw new Error("Missing username or password");
                 }
-                const existUser = await prisma.users.findUnique({
-                    where: {
-                        username: username
-                    }
-                })
+                let existUser;
+                try {
+                    existUser = await prisma.users.findUnique({
+                        where: {
+                            username: username
+                        }
+                    })
+                } catch (error) {
+                    throw new Error('Error while connecting to Database')
+                }
                 if (!existUser || !(await bcrypt.compare(password, existUser.password))) {
                     throw new Error("Invalid username or password");
                 }
-                return existUser;
+                return {
+                    id: existUser.id,
+                    fullname: existUser.fullname,
+                    username: existUser.username,
+                    role: existUser.role_id
+                };
             },
         }),
     ],
+    session: {
+        strategy: "jwt",
+        maxAge: 4 * 60 * 60, // 4 hours
+        updateAge: 24 * 60 * 60, // 24 hours
+    },
 }
